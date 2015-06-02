9b5e079b4c48c6e4dee64213b0d58aa00ce9f20f
David Halls <dahalls@gmail.com>
2015-06-02 07:38:22 +0100

    Update modules and rawgit links

--- docs.js
+++ docs.js
@@ -71,7 +71,7 @@ grunt test
 grunt coverage
 ```
 
-[Instanbul](http://gotwarlost.github.io/istanbul/) results are available [here](http://githubraw.herokuapp.com/davedoesdev/pub-keystore/master/coverage/lcov-report/index.html).
+[Instanbul](http://gotwarlost.github.io/istanbul/) results are available [here](http://rawgit.davedoesdev.com/davedoesdev/pub-keystore/master/coverage/lcov-report/index.html).
 
 Coveralls page is [here](https://coveralls.io/r/davedoesdev/pub-keystore).
 
